c218a02f41df30d5b178da375fbd678b168af590
Sama Jabri <sama.jabri@outlook.com>
2023-01-14 19:44:39 +0300

    Project Structure + Login / Sign Up Pages (Version 01)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link to="/login">
+        <Button>Login</Button>
+      </Link>
+      <Link to="/signup">
+        <Button>Sign Up</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
